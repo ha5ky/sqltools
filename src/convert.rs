fe7eebe73ba7691b449f239caf6a63202a524cb9
chun.zhang2 <chun.zhang2@geely.com>
2024-05-22 13:17:55 +0800

    feat: add MIT to files

--- src/convert.rs
+++ src/convert.rs
@@ -1,3 +1,24 @@
+// MIT License
+//
+// Copyright (c) 2024 hu5ky
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 use anyhow::{anyhow, Error};
 use sqlparser::ast::{Expr, Offset as SqlOffset, OffsetRows, Select, SetExpr, Statement, Value};
 
@@ -39,15 +60,15 @@ impl<'a> TryFrom<&'a Statement> for Sql<'a> {
 // pub struct Offset<'a>(pub(crate) &'a SqlOffset);
 pub struct Offset(pub(crate) SqlOffset);
 
-// impl<'a> From<Offset<'a>> for i64 { 
+// impl<'a> From<Offset<'a>> for i64 {
 //     fn from(offset: Offset) -> Self {
-//          match offset.0 { 
-//             SqlOffset { 
-//                 value: Expr::Value(Value::Number(v, _b)), 
-//                 .. 
-//             } => v.parse().unwrap_or(0), 
-//             _ => 0, 
-//         } 
+//          match offset.0 {
+//             SqlOffset {
+//                 value: Expr::Value(Value::Number(v, _b)),
+//                 ..
+//             } => v.parse().unwrap_or(0),
+//             _ => 0,
+//         }
 //     }
 // }
 impl<'a> From<Offset> for i64 {
